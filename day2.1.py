9f7de8ef4f2e0916dcb849c08ad1e132c4ced04c
“jason” <p1633038868@qq.com>
2021-05-26 09:53:26 +0800

    第二天day2.1.py

---
+++ day2.1.py
@@ -0,0 +1,318 @@
+
+
+
+# 日期	服装名称	价格/件	库存数量	销售量/每日
+# 1号	羽绒服	253.6	500	10
+# 2号	牛仔裤	86.3	600	60
+# 3号	风衣	96.8	335	43
+# 4号	皮草	135.9	855	63
+# 5号	T血	65.8	632	63
+# 6号	衬衫	49.3	562	120
+# 7号	牛仔裤	86.3	600	72
+# 8号	羽绒服	253.6	500	69
+# 9号	牛仔裤	86.3	600	35
+# 10号	羽绒服	253.6	500	140
+# 11号	牛仔裤	86.3	600	90
+# 12号	皮草	135.9	855	24
+# 13号	T血	65.8	632	45
+# 14号	风衣	96.8	335	25
+# 15号	牛仔裤	86.3	600	60
+# 16号	T血	65.8	632	129
+# 17号	羽绒服	253.6	500	10
+# 18号	风衣	96.8	335	43
+# 19号	T血	65.8	632	63
+# 20号	牛仔裤	86.3	600	60
+# 21号	皮草	135.9	855	63
+# 22号	风衣	96.8	335	60
+# 23号	T血	65.8	632	58
+# 24号	牛仔裤	86.3	600	140
+# 25号	T血	65.8	632	48
+# 26号	风衣	96.8	335	43
+# 27号	皮草	135.9	855	57
+# 28号	羽绒服	253.6	500	10
+# 29号	T血	65.8	632	63
+# 30号	风衣	96.8	335	78
+
+
+
+
+number1="1号"
+calmet1 = "羽绒服"
+price1 = 253.6
+stock1=500
+market1=10
+
+number2="2号"
+calmet2 = "牛仔裤"
+price2 = 86.3
+stock2=600
+market2=60
+
+
+number3="3号"
+calmet3 = "风衣"
+price3= 96.8
+stock3=335
+market3=43
+
+
+
+number4="4号"
+calmet4 = "皮草"
+price4= 135.9
+stock4=855
+market4=63
+
+
+
+number5="5号"
+calmet5 = "T恤"
+price5= 65.3
+stock5=632
+market5=63
+
+
+
+number6="6号"
+calmet6 = "衬衫"
+price6= 49.3
+stock6=562
+market6=120
+
+
+
+number7="7号"
+calmet7 = "牛仔裤"
+price7= 86.3
+stock7=600
+market7=35
+
+
+
+number8="8号"
+calmet8 = "羽绒服"
+price8= 253.6
+stock8=500
+market8=140
+
+
+
+number9="9号"
+calmet9 = "牛仔裤"
+price9= 86.3
+stock9=500
+market9=35
+
+
+
+number10="10号"
+calmet10 = "羽绒服"
+price10 = 253.6
+stock10=500
+market10=140
+
+
+
+number11="11号"
+calmet11 = "牛仔裤"
+price11 = 86.3
+stock11=600
+market11=90
+
+
+number12="12号"
+calmet12 = "皮草"
+price12 = 135.9
+stock12=855
+market12=24
+
+
+
+number13="13号"
+calmet13 = "T恤"
+price13 = 65.8
+stock13=632
+market13=45
+
+
+number14="14号"
+calmet14 = "风衣"
+price14 = 96.8
+stock14=335
+market14=25
+
+
+number15="15号"
+calmet15 = "牛仔裤"
+price15 = 86.3
+stock15=600
+market15=60
+
+
+number16="16号"
+calmet16 = "T恤"
+price16 = 65.8
+stock16=632
+market16=129
+
+number17="17号"
+calmet17 = "羽绒服"
+price17 = 253.6
+stock17=500
+market17=10
+
+number18="18号"
+calmet18 = "风衣"
+price18 = 96.8
+stock18=335
+market18=43
+
+number19="19号"
+calmet19 = "T恤"
+price19 = 65.8
+stock19=632
+market19=63
+
+number20="20号"
+calmet20 = "牛仔裤"
+price20= 86.3
+stock20=600
+market20=60
+
+number21="21号"
+calmet21 = "皮草"
+price21 = 135.9
+stock21=855
+market21=63
+
+number22="22号"
+calmet22 = "风衣"
+price22 = 96.8
+stock22=335
+market22=60
+
+number23="23号"
+calmet23 = "T恤"
+price23 = 65.8
+stock23=632
+market23=58
+
+number24="24号"
+calmet24= "牛仔裤"
+price24 = 86.3
+stock24=600
+market24=140
+
+number25="25号"
+calmet25 = "T恤"
+price25 = 65.8
+stock25=632
+market25=48
+
+number26="26号"
+calmet26 = "风衣"
+price26 = 96.8
+stock26=335
+market26=43
+
+number27="27号"
+calmet27 = "皮草"
+price27 = 135.9
+stock27=855
+market27=57
+
+number28="28号"
+calmet28 = "羽绒服"
+price28 = 253.6
+stock28=500
+market28=10
+
+number29="29号"
+calmet29 = "T恤"
+price29 = 65.8
+stock29=632
+market29=63
+
+number30="30号"
+calmet30 = "风衣"
+price30 = 96.8
+stock30=335
+market30=78
+
+print("日期","服装名称","价格/件","库存数量","销售量/每日")
+print(number1 ,"",calmet1 ,"",price1 ,"",stock1 ,"",market1 )
+print(number2 ,"",calmet2 ,"",price2 ,"",stock2 ,"",market2 )
+print(number3 ,"",calmet3 ,"",price3 ,"",stock3 ,"",market3 )
+print(number4 ,"",calmet4 ,"",price4 ,"",stock4 ,"",market4 )
+print(number5 ,"",calmet5 ,"",price5 ,"",stock5 ,"",market5 )
+print(number6 ,"",calmet6 ,"",price6 ,"",stock6 ,"",market6 )
+print(number7 ,"",calmet7 ,"",price7 ,"",stock7 ,"",market7 )
+print(number8 ,"",calmet8 ,"",price8 ,"",stock8 ,"",market8 )
+print(number9 ,"",calmet9 ,"",price9 ,"",stock9 ,"",market9 )
+print(number10,"",calmet10,"",price10,"",stock10,"",market10)
+print(number11,"",calmet11,"",price11,"",stock11,"",market11)
+print(number12,"",calmet12,"",price12,"",stock12,"",market12)
+print(number13,"",calmet13,"",price13,"",stock13,"",market13)
+print(number14,"",calmet14,"",price14,"",stock14,"",market14)
+print(number15,"",calmet15,"",price15,"",stock15,"",market15)
+print(number16,"",calmet16,"",price16,"",stock16,"",market16)
+print(number17,"",calmet17,"",price17,"",stock17,"",market17)
+print(number18,"",calmet18,"",price18,"",stock18,"",market18)
+print(number19,"",calmet19,"",price19,"",stock19,"",market19)
+print(number20,"",calmet20,"",price20,"",stock20,"",market20)
+print(number21,"",calmet21,"",price21,"",stock21,"",market21)
+print(number22,"",calmet22,"",price22,"",stock22,"",market22)
+print(number23,"",calmet23,"",price23,"",stock23,"",market23)
+print(number24,"",calmet24,"",price24,"",stock24,"",market24)
+print(number25,"",calmet25,"",price25,"",stock25,"",market25)
+print(number26,"",calmet26,"",price26,"",stock26,"",market26)
+print(number27,"",calmet27,"",price27,"",stock27,"",market27)
+print(number28,"",calmet28,"",price28,"",stock28,"",market28)
+print(number29,"",calmet29,"",price29,"",stock29,"",market29)
+print(number30,"",calmet30,"",price30,"",stock30,"",market30)
+
+money=price1*stock1+price2*stock2+price3*stock3+price4*stock4+price5*stock5+price6*stock6+price7*stock7+price8*stock8+price9*stock9+price10*stock10+price11*stock11+price12*stock12+price13*stock13+price14*stock14+price15*stock15+price16*stock16+price17*stock17+price18*stock18+price19*stock19+price20*stock20+price21*stock21+price22*stock22+price23*stock23+price24*stock24+price25*stock25+price26*stock26+price27*stock27+price28*stock28+price29*stock29+price30*stock30
+
+print("总金额：￥",money)
+print("羽绒服金额占比:",( round(price1*stock1/money*100,2)),"%")
+print("羽绒服每件占比：",(round(price1/money*100,2)),"%")
+# 销售额=销售量×平均销售价格
+
+print(market30*price30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2
+
+# 标识符	是否合法	标识符	是否合法
+# char	是	Cy%ty	否
+# Oax_li	是	$123	否
+# fLul	是	3_3 	是
+# BYTE	是	T_T	是
+
+
+
+
+
+
+# 第一题
+a = 56
+b = 78
+a,b=b,a
+print(a)
+print(b)
+
+
+
+
+
